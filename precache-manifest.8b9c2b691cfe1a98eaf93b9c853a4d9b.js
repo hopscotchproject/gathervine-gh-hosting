f526b5e18175e617c71f8259b93cab1da28896c5
Floyd Ding <floydding@gmail.com>
2020-10-11 11:38:51 -0400

    Updates

---
+++ precache-manifest.8b9c2b691cfe1a98eaf93b9c853a4d9b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e7dbae8bec9251ae276faf101a3f9f0",
+    "url": "/gathervine-gh-hosting/index.html"
+  },
+  {
+    "revision": "dfbeff15a86a9c1861ed",
+    "url": "/gathervine-gh-hosting/static/css/2.25b83785.chunk.css"
+  },
+  {
+    "revision": "dfbeff15a86a9c1861ed",
+    "url": "/gathervine-gh-hosting/static/js/2.365ed2a7.chunk.js"
+  },
+  {
+    "revision": "8becd5ebcb4af009a6e59da1c90501ec",
+    "url": "/gathervine-gh-hosting/static/js/2.365ed2a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f5c07d8569042bfb60b",
+    "url": "/gathervine-gh-hosting/static/js/main.bcd4b82a.chunk.js"
+  },
+  {
+    "revision": "a11dbc30157418400a23",
+    "url": "/gathervine-gh-hosting/static/js/runtime-main.1dbf0d16.js"
+  },
+  {
+    "revision": "2cd3ac16cfba99a60ce9dfde86583ef9",
+    "url": "/gathervine-gh-hosting/static/media/dance.2cd3ac16.png"
+  },
+  {
+    "revision": "dd443c1e20dc982495c9d7fae39659e6",
+    "url": "/gathervine-gh-hosting/static/media/games.dd443c1e.png"
+  },
+  {
+    "revision": "cd1bcf478f3d98132ab8d2e850aab3b7",
+    "url": "/gathervine-gh-hosting/static/media/math.cd1bcf47.png"
+  },
+  {
+    "revision": "8050950df2b00b338e415c734a60721d",
+    "url": "/gathervine-gh-hosting/static/media/music.8050950d.png"
+  },
+  {
+    "revision": "a54eb218830cbc2d96b57229bdb1b91b",
+    "url": "/gathervine-gh-hosting/static/media/paint.a54eb218.png"
+  },
+  {
+    "revision": "7063059653e279332a79e7c732c1fc61",
+    "url": "/gathervine-gh-hosting/static/media/sit.70630596.png"
+  },
+  {
+    "revision": "fe7d8a37aa7444129a2438fa1f6f270f",
+    "url": "/gathervine-gh-hosting/static/media/theater.fe7d8a37.png"
+  }
+]);
